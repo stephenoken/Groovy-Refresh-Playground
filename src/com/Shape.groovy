d09613f074b7f716e5e826af238b40e7c843c4f0
Stephen O'Kennedy <stephenoken@gmail.com>
2016-06-28 17:03:55 +0100

    "Initial Commit"
    ""

---
+++ src/com/Shape.groovy
@@ -0,0 +1,8 @@
+package com
+
+/**
+ * Created by stephenokennedy on 28/06/2016.
+ */
+interface Shape {
+    void draw();
+}
---
+++ src/com/Shapes.groovy
@@ -0,0 +1,29 @@
+package com
+
+/**
+ * Created by stephenokennedy on 28/06/2016.
+ */
+
+class Square implements Shape{
+
+    @Override
+    void draw() {
+        print("Drawing Square")
+    }
+}
+
+class Circle implements Shape{
+
+    @Override
+    void draw() {
+        print("Drawing Circle")
+    }
+}
+
+
+
+Shape s1 = new Square()
+Shape s2 = new Circle()
+
+s1.draw()
+s2.draw()
